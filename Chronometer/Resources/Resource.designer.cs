547f69156c375788dda84fc4c5fc0a93fbd02491
Devian <info@devian.tk>
2015-11-17 16:55:38 +0100

    add landscape layout

--- Chronometer/Resources/Resource.designer.cs
+++ Chronometer/Resources/Resource.designer.cs
@@ -60,11 +60,17 @@ namespace Chronometer
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int resetButton = 2131034114;
-			
 			// aapt resource value: 0x7f050001
-			public const int startStopButton = 2131034113;
+			public const int linearLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int linearLayout2 = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int resetButton = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int startStopButton = 2131034114;
 			
 			// aapt resource value: 0x7f050000
 			public const int timeView = 2131034112;
